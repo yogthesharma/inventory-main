338774cb62b33b736cc3785afdfa2013563cd2f2
Yog Sharma <yogthesharma@gmail.com>
2020-07-23 16:24:24 +0530

    1.0.7

--- server.js
+++ server.js
@@ -38,7 +38,11 @@ mongoose.connect(
   }
 );
 if (process.env.NODE_ENV === "production") {
-  app.use("*", express.static(path.join(__dirname, "client", "build")));
+  app.use(express.static("client/build"));
+
+  app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 // starting up the server
 app.listen(port, () => {
